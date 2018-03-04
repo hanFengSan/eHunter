bd6e9827bf69478e074b17cd17aeb7664c6a861d
yakami <c360785655@gmail.com>
2018-03-04 20:51:46 +0800

    test github url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
     version: '2.0',
-    updateServer1: 'https://www.alexskye.info:2333/update.json',
-    updateServer2: 'https://www.alexskye.info:2333/update.json'
+    updateServer1: 'https://raw.githubusercontent.com/hanFengSan/eHunter/master/update.json',
+    // updateServer1: 'https://www.alexskye.info:2333/update.json',
+    updateServer2: 'https://raw.githubusercontent.com/hanFengSan/eHunter/master/update.json'
 };
