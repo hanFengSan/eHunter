15c126dc7bc45983658fb5e3115f6c0a61876880
yakami <c360785655@gmail.com>
2018-03-05 20:38:26 +0800

    update popup

--- src/main.popup.js
+++ src/main.popup.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './app.popup.vue'
-import MuseUI from 'muse-ui'
-import 'src/style/muse-ui/index.less'
 
 Vue.use(VueResource)
-Vue.use(MuseUI)
 
 /* eslint-disable no-unused-vars */
 const app = new Vue({
