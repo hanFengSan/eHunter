f59a6dfc23d4bd5552995914deb47a2cce13ec69
yakami <c360785655@gmail.com>
2018-03-05 16:45:37 +0800

    update image urls in instructions

--- src/assets/value/instruction.js
+++ src/assets/value/instruction.js
@@ -3,16 +3,16 @@ import config from '../../config'
 export default {
     cn: `
 1.Change language/切换语言/言語を変更
-![language](http://0.0.0.0:8000/language.jpg)
+![image-language](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/language.jpg)
 
 1.显示/隐藏顶栏和关闭eHunter
-![topbar_close](http://0.0.0.0:8000/topbar_close.jpg)
+![image-topbar_close](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/topbar_close.jpg)
 
 2.在页面右上角点击打开eHunter
-![open_ehunter](http://0.0.0.0:8000/open_ehunter.jpg)
+![image-open_ehunter](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/open_ehunter.jpg)
 
 3.在\`阅读模式\`为\`书本\`时, 可使用键盘的左右键翻页
-![keyboard_arrow](http://0.0.0.0:8000/keyboard_arrow.jpg)
+![image-keyboard_arrow](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/keyboard_arrow.jpg)
 
 4.\`分卷页数\`对性能要求较高,请不要设置过高,可能会导致卡顿.
 
@@ -31,16 +31,16 @@ export default {
 `,
     en: `
 1.Change language/切换语言/言語を変更
-![language](http://0.0.0.0:8000/language.jpg)
+![image-language](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/language.jpg)
 
 1.Show/hide top bar and close the eHunter
-![topbar_close](http://0.0.0.0:8000/topbar_close.jpg)
+![image-topbar_close](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/topbar_close.jpg)
 
 2.Click the button at the upper right corner of this page to open the eHunter
-![open_ehunter](http://0.0.0.0:8000/open_ehunter.jpg)
+![image-open_ehunter](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/open_ehunter.jpg)
 
 3.When the \`Mode\` is \`Book\`, you can use the keyboard's left and right keys to page
-![keyboard_arrow](http://0.0.0.0:8000/keyboard_arrow.jpg)
+![image-keyboard_arrow](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/keyboard_arrow.jpg)
 
 4.This is a high performance requirements on \`Volume size\`. If too big, the program will be slow.
 
@@ -59,16 +59,16 @@ If you like this extension, I hope you can give a five-star rating in store. 8-)
 `,
     jp: `
 1.Change language/切换语言/言語を変更
-![language](http://0.0.0.0:8000/language.jpg)
+![image-language](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/language.jpg)
 
 1.トップバーを表示/非表示にしてeHunterを閉じる
-![topbar_close](http://0.0.0.0:8000/topbar_close.jpg)
+![image-topbar_close](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/topbar_close.jpg)
 
 2.このページの右上隅にあるボタンをクリックしてeHunterを開きます
-![open_ehunter](http://0.0.0.0:8000/open_ehunter.jpg)
+![image-open_ehunter](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/open_ehunter.jpg)
 
 3.\`モード\`が\`スクロール\`のときは、キーボードの左右のキーを使用してページングすることができます
-![keyboard_arrow](http://0.0.0.0:8000/keyboard_arrow.jpg)
+![image-keyboard_arrow](https://raw.githubusercontent.com/hanFengSan/eHunter/master/github_image/keyboard_arrow.jpg)
 
 4.これは\`ボリュームサイズ\`の高性能要件です。 大きすぎるとプログラムが遅くなります。
 
